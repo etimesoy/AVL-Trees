c52520b60c807e548d2a375a6b56901364522c22
Ruslan Gazizov <ruslangazizov36@gmail.com>
2021-04-10 13:10:53 +0300

    Add tests folder removing in case it already exists

--- download_data_from_dropbox.py
+++ download_data_from_dropbox.py
@@ -1,18 +1,22 @@
+from shutil import rmtree
 import subprocess
 import zipfile
 import os
 
 
 def main():
-    link_to_file = 'https://www.dropbox.com/s/kjodqw3tm7hc9vr/tests.zip'
-    if not os.path.exists('tests'):
-        os.mkdir('tests')
-        os.chdir('tests')
-        subprocess.Popen(['curl', '-L', '-o', 'tests.zip', link_to_file]).wait()
-        zip_file = zipfile.ZipFile('tests.zip')
-        zip_file.extractall()
-        zip_file.close()
-        os.remove('tests.zip')
+    link_to_zip_file = 'https://www.dropbox.com/s/ucvk77v43g7xhhz/tests.zip?dl=0'
+    zip_file_name = 'tests.zip'
+    tests_foler_name = 'tests'
+    if os.path.exists(tests_foler_name):
+        rmtree(tests_foler_name)
+    os.mkdir(tests_foler_name)
+    os.chdir(tests_foler_name)
+    subprocess.Popen(['curl', '-L', '-o', zip_file_name, link_to_zip_file]).wait()
+    zip_file = zipfile.ZipFile(zip_file_name)
+    zip_file.extractall()
+    zip_file.close()
+    os.remove(zip_file_name)
 
 
 if __name__ == '__main__':
